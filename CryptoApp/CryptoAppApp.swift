ff41baf786753acb347fdcdb18624d3f81ed1c77
Ulugbek <ulu96ek06@gmail.com>
2025-09-06 15:28:52 +0500

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Ulugbek Ilhomov on 06/09/25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
