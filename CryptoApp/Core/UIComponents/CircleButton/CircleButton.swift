797a175ccbc5d2f1cab7b1aaac0a0f0c3c8c79ea
Ulugbek <ulu96ek06@gmail.com>
2025-09-07 15:23:54 +0500

    Added Assets

---
+++ CryptoApp/Core/UIComponents/CircleButton/CircleButton.swift
@@ -0,0 +1,19 @@
+//
+//  CircleButton.swift
+//  CryptoApp
+//
+//  Created by Ulugbek Ilhomov on 06/09/25.
+//
+
+import SwiftUI
+
+struct CircleButton: View {
+    
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    CircleButton()
+}
